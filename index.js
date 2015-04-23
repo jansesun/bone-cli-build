03c50fc11dfab8429d7e66ff0d98fd5dc3fcf300
jansesun <307665751@qq.com>
2015-04-23 17:01:54 +0800

    modified index.js auto-increase version

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-var fs = require('fs');
-var path = require('path');
-var pkg = require('./package.json');
-
+var fs = require('fs'),
+	path = require('path'),
+	pkg = require('./package.json'),
+	timeMap = ['','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+function timestamp() {
+	// 组合时间戳
+	var time = new Date(),
+		year, month, date;
+	year = time.getFullYear() - 2000;
+	month = time.getMonth() + 1;
+	month = month.toString().length > 1 ? month : ('0' + month);
+	date = time.getDate();
+	date = date.toString().length > 1 ? date : ('0' + date);
+	return year + month + date;
+}
+function getTargetFile(file) {
+	var autoNameNum = 0,
+		ext = path.extname(file),
+		basename = path.basename(file, ext),
+		nameFlag;
+	while(bone.fs.existFile(file)) {
+		nameFlag = timeMap[Math.floor(autoNameNum / 26)];
+		nameFlag += timeMap[Math.floor(autoNameNum % 26)];
+		autoNameNum++;
+		file = basename + '_' + timestamp() + nameFlag + ext;
+	}
+	return file;
+}
 function buildFileArray(files, bone) {
 	files.forEach(function(file) {
+		var targetFile;
 		file = path.resolve(file);
+		targetFile = getTargetFile(file);
 		if(bone.fs.existFile(file, {notFs: true})) {
 			var readStream = bone.fs.createReadStream(file);
-			var writeStream = bone.fs.createWriteStream(file, {focus: true});
+			var writeStream = bone.fs.createWriteStream(targetFile , {focus: true});
 
 			readStream.pipe(writeStream, {end: false});
 			readStream.on('end', function() {
-				console.log('[build] '+file);
+				console.log('[build] ' + targetFile);
 			});
 		} else {
-			console.log('[warn] not exist '+file);
+			console.log('[warn] not exist '+ file);
 		}
 	});
 }
